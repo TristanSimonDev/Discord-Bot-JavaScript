88e980a18dc00d4ad1bd937878be246258a9fb67
Tristan | S. <simontristandev@gmail.com>
2024-09-04 22:15:35 +0200

    change loader

--- src/Bots/Bot1/Load/load.js
+++ src/Bots/Bot1/Load/load.js
@@ -1,44 +1,32 @@
-const Discord = require('discord.js');
-const SettingsJSON = require('../../../../.vscode/BotSettings/Settings.json')
+const Discord = require("discord.js");
+const SettingsJSON = require("../../../../.vscode/BotSettings/Settings.json");
 
 const emojis = Object.values(SettingsJSON.Reactions.ReactionRoleEmoji);
 
 const LoadReactions = async (Client) => {
-    
     if (Client instanceof Discord.Client) {
+        const reactionRoleChannel = Client.channels.cache.get(SettingsJSON.Channels["ReactionRole-Channel"]);
+        class LoadMessageWithReaction {
+            static LoadReactionRoleEmojis = async () => {
+                if (reactionRoleChannel instanceof Discord.TextChannel) {
+                    const message = await reactionRoleChannel.messages.fetch(SettingsJSON.Messages["ReactionRole-Message"]);
+                    const reactions = message.reactions.cache;
 
-        
-        const reactionRoleChannel = Client.channels.cache.get(SettingsJSON.Channels['ReactionRole-Channel']);
-             class LoadMessageWithReaction {
-                    static LoadReactionRoleEmojis = async () => {
-                        if (reactionRoleChannel instanceof Discord.TextChannel) { 
-                            const message = await reactionRoleChannel.messages.fetch(SettingsJSON.Messages['ReactionRole-Message']);
-                            const reactions = message.reactions.cache
+                    reactions.forEach(async (reaction) => {
+                        console.log(`Reaction: ${reaction.emoji.name}, Count: ${reaction.count}`);
+                    });
 
-                            reactions.forEach(async reaction => {
-                                console.log(`Reaction: ${reaction.emoji.name}, Count: ${reaction.count}`);
-                            });
-                
-                            for (const emoji of emojis) {
-                                await message.react(emoji);
-                            } 
+                    for (const emoji of emojis) {
+                        await message.react(emoji);
+                    }
+                }
+            };
+        }
 
-                        }
-                    }                    
-
-                              
-                 }
-            
-        
-
-        LoadMessageWithReaction.LoadReactionRoleEmojis()
-    } 
-};
-
-
-
-
+        LoadMessageWithReaction.LoadReactionRoleEmojis();
+    }
+}
 
 module.exports = {
-    LoadReactions
+    LoadReactions,
 };
