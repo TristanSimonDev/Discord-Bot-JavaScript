34ed6a6cbae47faba9329b7e2f877495a6b89af7
Tristan | S. <simontristandev@gmail.com>
2024-09-14 15:05:50 +0200

    make a function for the commands and export it

--- src/Bots/Bot1/Commands.js
+++ src/Bots/Bot1/Commands.js
@@ -1,32 +1,37 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const fs = require('node:fs');
+const { listenerCount } = require('node:process');
 const dotenv = require('dotenv').config()
 
-
-let commands = []
-const commandFiles = fs.readdirSync('./src/Bots/Bot1/cmd').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./cmd/${file}`);
-	if ('data' in command && 'execute' in command) {
-		commands.push(command.data.toJSON());
+function ListCommands() {
+	let commands = []
+	const commandFiles = fs.readdirSync('./src/Bots/Bot1/cmd').filter(file => file.endsWith('.js'));
+	
+	for (const file of commandFiles) {
+		const command = require(`./cmd/${file}`);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		}
 	}
+	
+	const rest = new REST().setToken(process.env.TokenForChatBot);
+	
+	(async () => {
+		try {
+			console.log('Started refreshing application (/) commands.');
+	
+			await rest.put(
+				Routes.applicationGuildCommands(process.env.ClientID, process.env.GuildID),
+				{ body: commands },
+			);
+	
+			console.log('Successfully reloaded application (/) commands.');
+		} catch (error) {
+			console.error(error);
+		}
+	})();
 }
 
-const rest = new REST().setToken(process.env.TokenForChatBot);
 
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			Routes.applicationGuildCommands(process.env.ClientID, process.env.GuildID),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
+module.exports = {ListCommands}
