9cdf5dcb602274588a2c53632743b59559c51cdf
Tristan | S. <simontristandev@gmail.com>
2024-09-18 21:26:14 +0200

    create ban.js for banning a user

---
+++ src/Bots/Bot1/cmd/Ban.js
@@ -0,0 +1,40 @@
+const Discord = require('../../../../modules/require').Discord
+const Emojis = require('../../../../modules/RequireBot1Settings').Emojis
+
+BanTimes = {
+    "1": 60,                //1min
+    "2": 180,               //3min
+    "3": 300,               //5min
+    "4": 600,               //10min
+    "5": 1800,              //30min
+    "6": 3600,              //60min[1h]
+    "7": 3600 * 6,          //6h
+    "8": 3600 * 12,         //12h
+    "9": 3600 * 24,         //24h[1d]
+    "10": 3600 * 24 * 3,    //3d
+    "11": 3600 * 24 * 7,    //7d[1week]
+    "12": 3600 * 6 * 30,    //30d[1month]
+    "13": 3600 * 6 * 182.5, //6Months
+    "14": 3600 * 6 * 365,   //365d[1Year]
+    "99": Infinity,         //Infinite
+}
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName("ban")
+        .setDescription("ban a user from the Server")
+        .addUserOption(option =>
+            option
+                .setName("target")
+                .setDescription("The User that you want to ban (ID)")
+                .setRequired(true)
+        ),
+    
+    
+    async execute(interaction) {
+        if (interaction instanceof Discord.CommandInteraction) {
+            const TargetID = interaction.options.get("target").value
+            const user = interaction.guild.members.fetch(TargetID)
+        }
+    }
+}
