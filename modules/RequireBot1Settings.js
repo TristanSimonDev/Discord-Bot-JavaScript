721798c4ab9baa86dd464b677b73778da3617b02
Tristan | S. <simontristandev@gmail.com>
2024-09-23 17:01:42 +0200

    export the channels in the modules

--- modules/RequireBot1Settings.js
+++ modules/RequireBot1Settings.js
@@ -1,6 +1,8 @@
 const Emojis = require('../.vscode/Bot1Settings/emojis.json')
+const Channels  = require('../.vscode/Bot1Settings/channels.json')
 
 
 module.exports = {
-    Emojis
+    Emojis,
+    Channels
 }
