a26d9e6ee1cbbe32de01ecc541195bbcc3de4b8e
Tristan | S. <simontristandev@gmail.com>
2024-09-24 15:33:01 +0200

    work on the system

--- src/Bots/Bot1/TicketSystem/TicketSystem.js
+++ src/Bots/Bot1/TicketSystem/TicketSystem.js
@@ -9,12 +9,13 @@ function SendTicketInstructions(Client) {
 
     if (Client instanceof Discord.Client) {
 
+        
         //get the TickedChannle
         const TicketChannel = Client.channels.cache.get(TicketChannelID)
 
         try {
 
-            TicketChannel.send({ embeds: [TicketEmbeds.InstructionEmbed()] })
+            TicketChannel.send({ embeds: [TicketEmbeds.InstructionEmbed().Embed], components: [TicketEmbeds.InstructionEmbed().Rows]  })
 
         } catch (err) {console.error(err)}
 
