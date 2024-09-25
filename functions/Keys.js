5f6c08b70843c5656d6926cdde2090406267b167
Tristan | S. <simontristandev@gmail.com>
2024-09-25 07:08:14 +0200

    create a key for Tickets

---
+++ functions/Keys.js
@@ -0,0 +1,25 @@
+const Chars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+function GenerateTicketKey() {
+    let lengthPerSequenze = 5 //for 5 it will be for example FHD5J
+    let Sequenzes = 5         //for 2 it would be FHD5J-FHD5J 
+        
+    let Key = ""
+    
+
+    for (let i = 0; i < Sequenzes; i++) {
+
+        Key.length > 1 ? Key += "-" : ""
+
+        for (let j = 0; j < lengthPerSequenze; j++) {
+            let RandomIndex = Math.floor(Math.random() * Chars.length)
+            Key = Key.concat(Chars[RandomIndex])
+        }
+        
+    }
+
+    return Key
+}
+
+module.exports = {GenerateTicketKey}
