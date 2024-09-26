3ae6fd314b9f16dfa9e8e7cfbe14c85643b5a869
Tristan | S. <simontristandev@gmail.com>
2024-09-26 19:20:54 +0200

    explain and delete the message after 5 seconds

--- src/Bots/Bot1/Interactions.js
+++ src/Bots/Bot1/Interactions.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js');
 const channels = require('../../../.vscode/Bot1Settings/channels.json')
 const GenerateTicketID = require('../../../functions/Keys').GenerateTicketKey
+const fs = require('fs');
+let TicketChannels = '.vscode/Bot1Settings/Tickets/TicketChannels.json'
 
+const data = fs.readFileSync(TicketChannels, 'utf-8')
+
+const ParsedTicketChannels = JSON.parse(data)
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
 async function SlashcommandInteraction(interaction) {
@@ -29,20 +38,23 @@ async function TicketButtonInteraction(interaction) {
 			"ticket-report-bug": ReportBug(interaction)
 		}	
 		
+		//Choose the Function to deal with
 		Types[interaction.customId]
 
-		async function ReportBug(interaction) {
-			
-			if (interaction instanceof Discord.ButtonInteraction) {
 
+		//first function
+		async function ReportBug(interaction) {
+			if (interaction instanceof Discord.ButtonInteraction) {
+				
+				//create channel
 				let TicketChannel = await interaction.guild.channels.create({
 					name: `ticket_${TicketID}`,
 					channels: [{ type: Discord.ChannelType.GuildText }],
 					reason: "Bug",
 					parent: "1287783226558906471"
 				})
-		
-
+				
+				//create Embed
 				const TicketEmbed = new Discord.EmbedBuilder() 
 					.setTitle(`New Ticket Created`)
 					.setDescription(
@@ -50,10 +62,23 @@ async function TicketButtonInteraction(interaction) {
 						`\nGlobalName: \`${interaction.user.globalName}\`` +
 						`\nUserID: \`${interaction.user.id}\`` +
 						`\n\nTickedID: \`${TicketID}\`` +
-						`\nTickedChannelID: \`${TicketChannel.id}\``
-					)
+						`\nTickedChannelID: \`${TicketChannel.id}\`` +
+						//get the Timestamp you need to do / 1000 to get the seconds beacause Typpicly you dont want to get the ms
+						`\n\nOpend on <t:${Math.floor(Date.now() / 1000)}>`) 
+					.setFooter({text: `Author: Tristan | S.`})
+					
+				
+				//write json
+				ParsedTicketChannels[TicketID] = TicketID
+				//send the TicketInfoEmbed
+				let ReplyWithTicketInfoEmbed = await interaction.reply({ embeds: [TicketEmbed] })
+				//wait 5 seconds
+				await sleep(5000)
+				//Delete the TicketInfoEmbed
+				ReplyWithTicketInfoEmbed.delete()
+				
 
-				interaction.reply({ embeds: [TicketEmbed] })
+				fs.writeFileSync(TicketChannels, JSON.stringify(ParsedTicketChannels, null, 2), 'utf-8');
 			}
  		}
 
