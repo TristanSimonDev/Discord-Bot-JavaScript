b4a4d6e24fef5cc341a4f5d95c5e76825dd3c1e9
Tristan | S. <simontristandev@gmail.com>
2024-09-26 21:48:58 +0200

    send only 1 instrunction

--- src/Bots/Bot1/TicketSystem/SendTicketIndroduction.js
+++ src/Bots/Bot1/TicketSystem/SendTicketIndroduction.js
@@ -4,8 +4,13 @@ const Discord = require('discord.js');
 const TicketChannelID = Settings.Channels.CreateTicketChannel
 const TicketEmbeds = require('./BuildTickedIntroductionEmbed')
 
-const fs = require('fs')
+const fs = require('fs');
+const { json } = require('express');
 const TicketSettings = '.vscode/Bot1Settings/Tickets/Ticket-Settings.json'
+
+const data = fs.readFileSync(TicketSettings, 'utf-8')
+
+let ParsedTicketSettings = JSON.parse(data)
 
 function SendTicketInstructions(Client) {
 
@@ -15,12 +20,21 @@ function SendTicketInstructions(Client) {
         //get the TickedChannle
         const TicketChannel = Client.channels.cache.get(TicketChannelID)
 
+        if (!ParsedTicketSettings.Settings["Instructions-In-Channel"]) {
+            try {
+
+                TicketChannel.send({ embeds: [TicketEmbeds.InstructionEmbed().Embed], components: [TicketEmbeds.InstructionEmbed().Rows] })
+                
+                //change the Sended Instruction to true to prevent multiple Instrunctions
+                ParsedTicketSettings.Settings["Instructions-In-Channel"] = true
+                fs.write(TicketSettings, JSON.stringify(ParsedTicketSettings, null, 2))
+
+            } catch (err) {console.error(err)}
+        } else {console.warn("Ticketinstruction is already in thhe channel")}
         //send the Embed
-        try {
+        
 
-            TicketChannel.send({ embeds: [TicketEmbeds.InstructionEmbed().Embed], components: [TicketEmbeds.InstructionEmbed().Rows]  })
-
-        } catch (err) {console.error(err)}
+        
 
     }
 
