8e33187a20fc2180a44b329b4a68067c68480a04
Tristan | S. <simontristandev@gmail.com>
2024-09-30 17:38:43 +0200

    working on dynamic access

--- src/Bots/testbot/index.js
+++ src/Bots/testbot/index.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const Discord = require('discord.js')
 const env = require('dotenv').config()
 
@@ -12,7 +13,10 @@ const EventFiles = fs.readdirSync("src/Bots/testbot/Events").filter(file => file
 for (const file of EventFiles) {
     const Event = require(EventFolder + file)
     console.log(EventFolder + file)
-    Event.execute(client)
+    try {
+        Event.Type == 'once' ? client.once(Event.name, (...args) => Event.execute(...args)) : client.on(Event.name, (...args) => Event.execute(...args))
+    } catch (err) {console.error(`Error: ${err}`)}
+    
 }
 
 
