5f07344c216e686a17c5bd5596300d0883f5320e
Tristan | S. <simontristandev@gmail.com>
2024-09-30 15:17:32 +0200

    require files dynamicly

--- src/Bots/testbot/index.js
+++ src/Bots/testbot/index.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js')
 const env = require('dotenv').config()
 
+const fs = require('fs')
+
 const client = new Discord.Client({ intents: [32767] });
 
-client.on('ready', client => {
-    console.log(`${client.user.displayName} is ready`)
-})
+const EventFolder = "./Events/"
 
-//console.log(Object.values(Discord.Events))
+const EventFiles = fs.readdirSync("src/Bots/testbot/Events").filter(file => file.endsWith('.js'))
+
+for (const file of EventFiles) {
+    const Event = require(EventFolder + file)
+    console.log(EventFolder + file)
+    Event.execute(client)
+}
+
+
+
+
 
 client.login(process.env.TokenForTestBot)
