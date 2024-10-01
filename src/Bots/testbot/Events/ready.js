25812c54f3a5bf8b4fdcb305be7b1adfed5f79c5
Tristan | S. <simontristandev@gmail.com>
2024-10-01 16:01:29 +0200

    just a line break xD

--- src/Bots/testbot/Events/ready.js
+++ src/Bots/testbot/Events/ready.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 module.exports = {
     name: Discord.Events.ClientReady,
     Type: 'once',
+    
     async execute(client) {
         console.log(client)
     }
