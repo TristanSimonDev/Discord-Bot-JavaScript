5057d3bfa601845f2150632262df5d583efcc5f0
Tristan | S. <simontristandev@gmail.com>
2024-10-01 15:55:59 +0200

    log the client out

--- src/Bots/testbot/Events/ready.js
+++ src/Bots/testbot/Events/ready.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js')
-const { once } = require('ws')
 
 module.exports = {
     name: Discord.Events.ClientReady,
     Type: 'once',
     async execute(client) {
-        console.log("test")
+        console.log(client)
     }
 }
