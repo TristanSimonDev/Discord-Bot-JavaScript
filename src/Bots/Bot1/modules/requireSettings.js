f870394b0e3a120207770abaddca0793b2403811
Tristan | S. <simontristandev@gmail.com>
2024-10-01 15:53:03 +0200

    make the modules directly in the Bot

---
+++ src/Bots/Bot1/modules/requireSettings.js
@@ -0,0 +1,8 @@
+const Emojis = require('../../../../.vscode/Bot1Settings/emojis.json')
+const Channels = require('../../../../.vscode/Bot1Settings/channels.json')
+
+
+module.exports = {
+    Emojis,
+    Channels
+}
