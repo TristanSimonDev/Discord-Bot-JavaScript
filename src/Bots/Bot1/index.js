b765aade4affde8f15edbea3f07739d95f546036
Tristan | S. <simontristandev@gmail.com>
2024-10-02 17:08:39 +0200

    use color output

--- src/Bots/Bot1/index.js
+++ src/Bots/Bot1/index.js
@@ -6,6 +6,8 @@ const interactions = require('./Interactions')
 
 const fs = require('fs')
 
+const ColorOutput = require('../../../functions/ColorOutput').ColorOutput
+
 
 const client = new Discord.Client({ intents: [32767], });  // Adjust intents as needed for your bot's functionality
 
@@ -35,19 +37,19 @@ const EventFiles = fs.readdirSync('src/Bots/Bot1/Events/').filter(file => file.e
 for (const file of EventFiles) {
     const Event = require('./Events/' + file)
 
-    console.log(`Event [${Event.Name}]-[ID: ${Event.EventID}] successfully loaded Dynamically`)
+    console.log(ColorOutput(`Event [${Event.Name}]-[ID: ${Event.EventID}] successfully loaded Dynamically`).green)
 
     try {
         
          client[Event.Type](Event.Name, async (...args) => Event.execute(...args))
         
-    } catch (err) {console.error(`Error: ${err}`)}
+    } catch (err) {console.log(ColorOutput(`Error: ${err}`).red)}
 }
 
 //---------------------------------------------------------------------
 
 process.on('SIGINT', () => {
-    console.log('Received SIGINT. Gracefully shutting down...');
+    console.log(ColorOutput('Received SIGINT. Gracefully shutting down...').green);
     client.destroy();  // Clean up resources and logout
     process.exit(0);  // Exit the process
 });
