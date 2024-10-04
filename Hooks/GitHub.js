dfb7777fcd9c9113b7b732508fa44d92fb07ffd4
Tristan | S. <simontristandev@gmail.com>
2024-10-04 21:15:07 +0200

    use payload and configure it #v3

--- Hooks/GitHub.js
+++ Hooks/GitHub.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js')
 
 const env = require('dotenv').config
 
+const payload = require('./payload.json')
+const fs = require('fs')
+
+const data = fs.readFileSync(payload, 'utf-8')
+
+console.log(data)
+
 const webhookClient = new Discord.WebhookClient({ id: "1291837786617544836", token: process.env.WebHookToken });
 
 webhookClient.send("test")
