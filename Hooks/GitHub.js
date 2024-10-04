5f942bc21acec9a2101145f9ffd5f90e4136d1cc
Tristan | S. <simontristandev@gmail.com>
2024-10-04 21:24:43 +0200

    use the payload as a string #v4

--- Hooks/GitHub.js
+++ Hooks/GitHub.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js')
 
-const env = require('dotenv').config
+const env = require('dotenv').config()
 
-const payload = require('./payload.json')
+
+
 const fs = require('fs')
 
-const data = fs.readFileSync(payload, 'utf-8')
+const data = fs.readFileSync('Hooks/payload.json', 'utf-8');
 
-console.log(data)
+const payload = JSON.parse(data);
+
+console.log(payload.test)
+
 
 const webhookClient = new Discord.WebhookClient({ id: "1291837786617544836", token: process.env.WebHookToken });
 
-webhookClient.send("test")
+webhookClient.send(payload.test.toString())
