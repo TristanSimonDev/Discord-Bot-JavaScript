523cb6bee79ac1e5f0bc47a713e93e452593e1ac
Tristan | S. <simontristandev@gmail.com>
2024-10-05 11:31:39 +0200

    write the message id in the file and when u have it edit the message via id

---
+++ Hooks/GitHub-total.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+
+const env = require('dotenv').config()
+
+const Timestamp = Math.floor(Date.now() / 1000)
+
+
+const fs = require('fs');
+
+const data = fs.readFileSync('Hooks/payload.json', 'utf-8');
+
+const payload = JSON.parse(data);
+
+
+const HourlyEmbed = new Discord.EmbedBuilder()
+    .setTitle("Commits \`All Time\`")
+    .setDescription(
+        `\nCurrent GitHub commits on my repo. This will automatically update every hour.` +
+        `\nCommits: \`${payload.Commits.toString()}\`` +
+        `\nLast Refreshed: <t:${Timestamp}:R>`)
+    
+
+
+
+const webhookClient = new Discord.WebhookClient({ id: "1291837786617544836", token: process.env.WebHookToken });
+
+if (payload["MessageID"]) {
+    try {
+        webhookClient.editMessage(payload["MessageID"], {embeds: [HourlyEmbed]})
+    } catch (err) {console.error(err)}
+} else {
+    webhookClient.send({ embeds: [HourlyEmbed] }).then(
+        message => {
+            payload["MessageID"] = message.id,
+        
+                console.log(payload["MessageID"]),
+    
+                fs.writeFileSync('Hooks/payload.json', JSON.stringify(payload, null, 4), 'utf-8')
+          
+    })
+}
+
+
+
+
