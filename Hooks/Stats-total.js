7ac3e3b49f0f2062ed704bd72e6a9a2549fc945d
Tristan | S. <simontristandev@gmail.com>
2024-10-06 13:19:12 +0200

    make it a function and export it

--- Hooks/Stats-total.js
+++ Hooks/Stats-total.js
@@ -1,6 +1,6 @@
 const env = require('dotenv').config()
 const fs = require('fs')
-const Timestamp = Math.floor(Date.now() / 1000)
+
 const Discord = require('discord.js')
 
 const PayloadPath = "Hooks/Data/payload.json"
@@ -10,8 +10,10 @@ const parsedPayload = JSON.parse(data)
 
 const WebhookClient = new Discord.WebhookClient({ id: "1292233165062275092", token: process.env.StatsWebhookToken })
 
+function updateStatsEmbed() {
+    const Timestamp = Math.floor(Date.now() / 1000)
 
-const statsEmbed = new Discord.EmbedBuilder()
+    const statsEmbed = new Discord.EmbedBuilder()
     .setTitle(`Server Stats`)
     .setDescription(
         `Total Messages: \`${parsedPayload["TotalMessages"]}\`\n` +
@@ -19,18 +21,17 @@ const statsEmbed = new Discord.EmbedBuilder()
         `Total GuildMembers: \`${parsedPayload["GuildMembers"]}\``
     )
 
-if (parsedPayload["StatsMessageID"]) {
+    if (parsedPayload["StatsMessageID"]) {
     WebhookClient.editMessage(parsedPayload["StatsMessageID"], { embeds: [statsEmbed] })
-} else {
-    WebhookClient.send({ embeds: [statsEmbed] }).then(
-        message => {
-            parsedPayload["StatsMessageID"] = message.id
-            console.log(parsedPayload["StatsMessageID"])
-            fs.writeFileSync(PayloadPath, JSON.stringify(parsedPayload, null, 4), 'utf-8')
-        }
-    )
+    } else {
+        WebhookClient.send({ embeds: [statsEmbed] }).then(
+            message => {
+                parsedPayload["StatsMessageID"] = message.id
+                console.log(parsedPayload["StatsMessageID"])
+                fs.writeFileSync(PayloadPath, JSON.stringify(parsedPayload, null, 4), 'utf-8')
+            }
+        )
+    }
 }
 
-
-
-
+module.exports = {updateStatsEmbed}
