93c621c9059c1a5b7ca1e1dce9a02817597f94b1
Tristan | S. <simontristandev@gmail.com>
2024-10-06 10:13:33 +0200

    make a file for data, for exaple using slash commonds or send a message or a buttoninteraction

---
+++ src/Bots/Bot1/Connection/Counters/Counters.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const PayloadPath = "Hooks/Data/payload.json"
+
+const data = fs.readFileSync(PayloadPath, 'utf-8')
+const ParsedPayload = JSON.parse(data)
+
+function increaseSlashcommandCounter() {
+    console.log(ParsedPayload)
+
+    ParsedPayload["TotalSlashCommands"] += 1
+
+    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
+}
+
+function increaseTotalMessageCounter() {
+    console.log(ParsedPayload)
+
+    ParsedPayload["TotalMessages"] += 1
+
+    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
+}
+
+
+increaseSlashcommandCounter()
