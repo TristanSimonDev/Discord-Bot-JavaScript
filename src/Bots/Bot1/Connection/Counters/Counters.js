87fb3013b056acd0aea675e19b763f6e964c12c7
Tristan | S. <simontristandev@gmail.com>
2024-10-07 22:31:42 +0200

    write the basic system of the Counter[dynamic] function

--- src/Bots/Bot1/Connection/Counters/Counters.js
+++ src/Bots/Bot1/Connection/Counters/Counters.js
@@ -4,9 +4,10 @@ const PayloadPath = "Hooks/Data/payload-stats.json"
 const data = fs.readFileSync(PayloadPath, 'utf-8')
 const ParsedPayload = JSON.parse(data)
 
-function increaseTotalChatInputCommandInteractionCounter() {
+function increaseTotalChatInputCommandInteractionCounter(constructorName) {
     console.log(ParsedPayload)
-
+    console.log(constructorName)
+    
     ParsedPayload["TotalSlashCommands"] += 1
 
     fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
@@ -21,11 +22,14 @@ function increaseTotalButtonInteractionCounter() {
     fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
 }
 
+function increaseStatCounters(constructorName) {
+
+    ParsedPayload["Total" + constructorName + "s"] += 1
+
+    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
+}
+
 module.exports = {
     increaseTotalChatInputCommandInteractionCounter,
     increaseTotalButtonInteractionCounter
 }
-/*
-increaseTotalButtonInteractionCounter
-increaseTotalTotalButtonInteractionCounter
-*/
