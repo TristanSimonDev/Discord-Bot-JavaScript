7667a4b73adee2da1a10d892a6672ca35d09f76c
Tristan | S. <simontristandev@gmail.com>
2024-10-07 09:48:31 +0200

    rewrite counters

--- src/Bots/Bot1/Connection/Counters/Counters.js
+++ src/Bots/Bot1/Connection/Counters/Counters.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const PayloadPath = "Hooks/Data/payload.json"
+const PayloadPath = "Hooks/Data/payload-stats.json"
 
 const data = fs.readFileSync(PayloadPath, 'utf-8')
 const ParsedPayload = JSON.parse(data)
 
-function increaseSlashcommandCounter() {
+function increaseTotalChatInputCommandInteractionCounter() {
     console.log(ParsedPayload)
 
     ParsedPayload["TotalSlashCommands"] += 1
@@ -12,24 +12,9 @@ function increaseSlashcommandCounter() {
     fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
 }
 
-function increaseTotalMessageCounter() {
+function increaseTotalButtonInteractionCounter() {
     console.log(ParsedPayload)
 
-    ParsedPayload["TotalMessages"] += 1
-
-    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
-}
-
-function increaseTotalMessageCounter() {
-    console.log(ParsedPayload)
-
-    ParsedPayload["TotalMessages"] += 1
-
-    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
-}
-
-function increaseTotalButtonInteractionsCounter() {
-    console.log(ParsedPayload)
 
     ParsedPayload["TotalButtonInteractions"] += 1
 
@@ -37,7 +22,10 @@ function increaseTotalButtonInteractionsCounter() {
 }
 
 module.exports = {
-    increaseSlashcommandCounter,
-    increaseTotalButtonInteractionsCounter,
-    increaseTotalMessageCounter
+    increaseTotalChatInputCommandInteractionCounter,
+    increaseTotalButtonInteractionCounter
 }
+/*
+increaseTotalButtonInteractionCounter
+increaseTotalTotalButtonInteractionCounter
+*/
