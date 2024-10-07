b9fe8b39aa29ded0e44ab9e7cf318b537131db7e
Tristan | S. <simontristandev@gmail.com>
2024-10-07 22:32:43 +0200

    delete the other functions

--- src/Bots/Bot1/Connection/Counters/Counters.js
+++ src/Bots/Bot1/Connection/Counters/Counters.js
@@ -4,25 +4,10 @@ const PayloadPath = "Hooks/Data/payload-stats.json"
 const data = fs.readFileSync(PayloadPath, 'utf-8')
 const ParsedPayload = JSON.parse(data)
 
-function increaseTotalChatInputCommandInteractionCounter(constructorName) {
-    console.log(ParsedPayload)
-    console.log(constructorName)
-    
-    ParsedPayload["TotalSlashCommands"] += 1
-
-    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
-}
-
-function increaseTotalButtonInteractionCounter() {
-    console.log(ParsedPayload)
-
-
-    ParsedPayload["TotalButtonInteractions"] += 1
-
-    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
-}
 
 function increaseStatCounters(constructorName) {
+
+    console.log(ParsedPayload)
 
     ParsedPayload["Total" + constructorName + "s"] += 1
 
