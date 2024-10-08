824c097c04e6a2c46e5f72ab96e01b51a8abc40a
Tristan | S. <simontristandev@gmail.com>
2024-10-08 19:43:56 +0200

    update

--- src/Bots/Bot1/Stats-WebHook/Counters.js
+++ src/Bots/Bot1/Stats-WebHook/Counters.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const PayloadPath = "src/Bots/Bot1/Hooks/payload-stats.json"
+const PayloadPath = "src/Bots/Bot1/Stats-WebHook/payload-stats.json"
 
 const data = fs.readFileSync(PayloadPath, 'utf-8')
 const ParsedPayload = JSON.parse(data)
