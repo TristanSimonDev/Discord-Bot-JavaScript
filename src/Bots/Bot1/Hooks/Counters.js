c7dbe4fa3ed3ef6cfea2e6c953218ac65054ebd9
Tristan | S. <simontristandev@gmail.com>
2024-10-08 19:17:15 +0200

    move counter

---
+++ src/Bots/Bot1/Hooks/Counters.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const PayloadPath = "src/Bots/Bot1/Hooks/payload-stats.json"
+
+const data = fs.readFileSync(PayloadPath, 'utf-8')
+const ParsedPayload = JSON.parse(data)
+
+
+function increaseStatCounters(constructorName) {
+
+    console.log(ParsedPayload)
+
+    const PayloadIndex = "Total" + constructorName + "s"
+
+    ParsedPayload[PayloadIndex] += 1
+
+    console.log(`Successfully wrote data <---${PayloadIndex}---> in ${PayloadPath}`)
+
+    fs.writeFileSync(PayloadPath, JSON.stringify(ParsedPayload, null, 4), 'utf-8')
+}
+
+module.exports = {
+    increaseStatCounters
+}
