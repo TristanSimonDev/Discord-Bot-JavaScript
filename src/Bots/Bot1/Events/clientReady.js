3d2494596ce09ce91ee00a93dc85cb57b88a8233
Tristan | S. <simontristandev@gmail.com>
2024-10-08 19:19:59 +0200

    use the stats-total update function to update the webhook

--- src/Bots/Bot1/Events/clientReady.js
+++ src/Bots/Bot1/Events/clientReady.js
@@ -1,28 +1,26 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
-const TicketInstruction = require('../Ticket-Instructions')
-const reloadcommands = require('../Commands')
-const ReactionLoader = require('../Load/load')
+const TicketInstruction = require("../Ticket-Instructions");
+const reloadcommands = require("../Commands");
+const ReactionLoader = require("../Load/load");
 
-
+const webhook = require("../Stats-WebHook/Stats-total");
 
 module.exports = {
     Name: Discord.Events.ClientReady,
-    Type: 'on',
+    Type: "on",
     EventID: 1,
     async execute(client) {
-        
         if (!(client instanceof Discord.Client)) return;
 
-        console.log("Client ready")
-        
-        TicketInstruction.InstructionEmbed(client)
-    
-        reloadcommands.ListCommands()
-    
-        ReactionLoader.LoadReactions(client)
-        
-        
-    }
-}
-    
+        console.log("Client ready");
+
+        TicketInstruction.InstructionEmbed(client);
+
+        reloadcommands.ListCommands();
+
+        ReactionLoader.LoadReactions(client);
+
+        setInterval(webhook.updateStatsEmbed, 10000);
+    },
+};
