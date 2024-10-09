f5fe760b777330761a3f3af3a98f3709e6182da0
Tristan | S. <simontristandev@gmail.com>
2024-10-09 16:34:05 +0200

    add color log

--- src/Bots/Bot1/Stats-WebHook/Stats-total.js
+++ src/Bots/Bot1/Stats-WebHook/Stats-total.js
@@ -1,7 +1,7 @@
 const env = require('dotenv').config()
 const fs = require('fs')
-
 const Discord = require('discord.js')
+const colorOutput = require('../functions/ColorOutput').ColorOutput
 
 const PayloadPath = "src/Bots/Bot1/Stats-WebHook/payload-stats.json"
 
@@ -11,7 +11,7 @@ const WebhookClient = new Discord.WebhookClient({ id: "1292233165062275092", tok
 
 async function updateStatsEmbed() {
 
-    console.log("Refreshing Stats Embed...")
+    console.log(colorOutput("Refreshing Stats Embed...").yellow)
 
     const data = fs.readFileSync(PayloadPath, 'utf-8')
     const parsedPayload = JSON.parse(data)
@@ -31,7 +31,7 @@ async function updateStatsEmbed() {
         if (parsedPayload["StatsMessageID"]) {
             // Update the existing message
             await WebhookClient.editMessage(parsedPayload["StatsMessageID"], { embeds: [statsEmbed] });
-            console.log("Successfully updated the existing Stats Embed in your channel!");
+            console.log(colorOutput("Successfully updated the existing Stats Embed in your channel!").green);
         } else {
             // Send a new message
             const message = await WebhookClient.send({ embeds: [statsEmbed] });
